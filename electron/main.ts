de8d6427e64cd493af5b78811ecf21046ad097dc
ztimson <zaktimson@gmail.com>
2019-07-08 22:22:59 -0400

    Added electron

---
+++ electron/main.ts
@@ -0,0 +1,35 @@
+import { app, BrowserWindow } from 'electron'
+import * as path from 'path'
+import * as url from 'url'
+
+let win: BrowserWindow;
+
+function createWindow() {
+  win = new BrowserWindow({
+    title: 'Formula Manager',
+    width: 1000,
+    height: 800
+  });
+
+  win.loadURL(
+    url.format({
+      pathname: path.join(__dirname, `/../../dist/FormulaManager2/index.html`),
+      protocol: 'file:',
+      slashes: true,
+    })
+  );
+
+  win.webContents.openDevTools();
+
+  win.on('closed', () => {
+    win = null
+  })
+}
+
+app.on('ready', createWindow);
+
+app.on('activate', () => {
+  if (win === null) {
+    createWindow();
+  }
+});
